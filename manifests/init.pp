8a8a85843f998d170886d8e63cd00b114ba3dc60
CookieMonster <me@me.com>
2021-03-18 18:56:37 -0700

    more config

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,18 +6,17 @@
 #   include elk
 ##elk/manifests/init.pp
 class elk {
-  class {'logstash':}
-  logstash::plugin {'logstash-input-beats':}
+  class { 'logstash': }
+  logstash::plugin { 'logstash-input-beats': }
   logstash::configfile{'beats':
     source => 'puppet:///modules/elk/beats.conf',
   }
 
 
   include ::java
-  class { 'elasticsearch':
-    jvm_options => ['-Xms256m','-Xmx256m']
-  }
-
+  class { 'elasticsearch': }
+#  jvm_options => ['-Xms256m','-Xmx256m']
+  
   class {'kibana':
     config => {
       'server.host' => '0.0.0.0'
