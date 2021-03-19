824e7d92efafcdfec07cff46f94fbf6e241738e3
CookieMonster <me@me.com>
2021-03-18 22:17:57 -0700

    add in filebeat info

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -4,7 +4,10 @@
 #
 # @example
 #   include elk::filebeat
-class elk::filebeat {
+class elk::filebeat (
+  $logstash_server = '127.0.0.1',
+  $logstash_port = '5044',
+) {
   include elastic_stack::repo
 
   package{'filebeat':
@@ -14,5 +17,13 @@ class elk::filebeat {
     ensure  => running,
     enable  => true,
     require => Package['filebeat'],
+  }
+  file{'/etc/filebeat/filebeat.yml':
+    ensure  => file,
+    content => epp('elk/filebeat.yml.epp', {
+      logstash_server => $logstash_server,
+      logstash_port   => $logstash_port,
+      }
+    )
   }
 }
