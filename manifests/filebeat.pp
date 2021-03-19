1ecead9769c540a12dacbaea91c9995d5eda7f94
CookieMonster <me@me.com>
2021-03-19 01:46:26 -0700

    updated filebeats manifest

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -4,9 +4,20 @@
 #
 # @example
 #   include elk::filebeat
+# Filebeat prospectors - expects array of structured data
+# [ {
+#   "type" => "log",
+#   "paths" => [
+#      "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
+#      "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
+#    ],
+#   "json_keys_under_root" => true
+# } ]
+
 class elk::filebeat (
-  $logstash_server = '127.0.0.1',
-  $logstash_port = '5044',
+  $prospectors      = [],
+  $logstash_server  = '127.0.0.1',
+  $logstash_port    = '5044',
 ) {
   include elastic_stack::repo
 
@@ -21,6 +32,7 @@ class elk::filebeat (
   file{'/etc/filebeat/filebeat.yml':
     ensure  => file,
     content => epp('elk/filebeat.yml.epp', {
+      prospectors     => $prospectors,
       logstash_server => $logstash_server,
       logstash_port   => $logstash_port,
       }
